01277b8ae4ed6b77dc85d2d67ebee2196128f35c
Ackmeh <121096857+Ackmeh@users.noreply.github.com>
2023-04-15 19:05:43 +0200

    My graded exam
    I Hope you give me full points

---
+++ MA_Graded.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Notebook 
+
+# Welcome to my Data Science notebook! In this notebook, I will explore various data science concepts and techniques using different tools and libraries.
+
+# ## Data Science Languages
+
+# · Python
+# · R
+# · C++
+# · Julia
+# · Java
+
+# ## Data Science Libraries
+
+# · Pandas
+# · NumPy
+# · Scikit-learn
+# · Matplotlib
+# · Seaborn
+# · ggplot2
+
+# ## Data Science Tools
+
+# | Category | Tool | Desciption |
+# | -------- | -------- | -------- |
+# | Data Analysis | Pandas | Data manipulation and analysis library |
+# |  | NumPy | Numerical computing library |
+# |  | SQL | Relational database management system |
+# | Data Visualization | Matplotlib | Data visualization library |
+# |  | Seaborn | Data visualization library |
+# |  | ggplot2 | ggplot2 |
+# | Machine Learning | Scikit-learn | Machine learning library |
+# |  | caret | Machine learning library for R |
+# | Deep Learning | TensorFlow | Deep learning library |
+# |  | Keras | Deep learning library |
+
+# In the following code cells, I will demonstrate some basic arithmetic operations using Python.
+
+# In[1]:
+
+
+# Multiplication and addition
+x = 5
+y = 3
+z = x * y + 2
+print(z)
+
+
+# In[2]:
+
+
+# Convert minutes to hours
+minutes = 120
+hours = minutes / 60
+print(hours)
+
+
+# ## Objectives
+
+# · Learn data science concepts and techniques
+# · Explore different data science tools and libraries
+# · Apply data science skills to real-world Problems
+
+# ## Author 
+
+# M.A.
+
+# In[ ]:
+
+
+
+
